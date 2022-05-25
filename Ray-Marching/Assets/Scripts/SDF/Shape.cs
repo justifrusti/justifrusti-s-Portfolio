8ca11123ee40621b35a13e3b8a3a639fbbb0ee8e
justifrusti <70446225+justifrusti@users.noreply.github.com>
2022-05-25 16:04:23 +0200

    Added Raymarching

---
+++ Ray-Marching/Assets/Scripts/SDF/Shape.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public enum ShapeType { Sphere, Cube, Torus};
+    public enum Operations { None, Blend, Cut, Mask};
+
+    public ShapeType shapeType;
+    public Operations operations;
+    public Color color = Color.white;
+    [Range(0, 1)]
+    public float blendStrength;
+    [HideInInspector]
+    public int numChildren;
+
+    public Vector3 Position
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+
+    public Vector3 Scale
+    {
+        get
+        {
+            Vector3 parentScale = Vector3.one;
+
+            if (transform.parent != null && transform.parent.GetComponent<Shape>() != null)
+            {
+                parentScale = transform.parent.GetComponent<Shape>().Scale;
+            }
+
+            return Vector3.Scale(transform.localScale, parentScale);
+        }
+    }
+}
